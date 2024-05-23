51c3e60b4680a1262bbeb37a46667be91a7599ab
edgarmorales <dev.edgarmorales@gmail.com>
2024-05-23 11:05:28 -0600

    Add - Se cambia settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Running migrations...'
-python manage.py makemigration && python manage.py migrate
+python manage.py makemigrations && python manage.py migrate
 
 echo 'Running command...'
 python manage.py importar_restaurantes restaurantes.csv
