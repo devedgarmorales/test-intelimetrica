5bba34cb5e164af6403144fd0160ce8133eb3417
edgarmorales <dev.edgarmorales@gmail.com>
2024-05-23 10:38:42 -0600

    Fix - Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
-
 echo 'Applying migrations...'
-python manage.py wait_for_db --settings=config.settings.production
-python manage.py migrate --settings=config.settings.production
+python manage.py makemigration && python manage.py migrate
 
 echo 'Run Command...'
 python manage.py importar_restaurantes restaurantes.csv
